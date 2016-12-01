b2691ead60f612c961413f597d981ab99787abbf
bcollin <bcollin@BCOLLIN2012>
2016-12-01 15:36:45 -0800

    add sample cmd line app

---
+++ sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let me know if you have a questions");
+
+            Console.WriteLine("Never mind");
+        }
+    }
+}
